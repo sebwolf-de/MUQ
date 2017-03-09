c66a32093c6f6c0d0bced92ef6bbd36fcfadd161
Matt Parno <matthew.d.parno@erdc.dren.mil>
2017-03-09 08:58:21 -0500

    Added ability to toggle compilation of compile groups.

--- cmake/CompileGroup.cmake
+++ cmake/CompileGroup.cmake
@@ -1,5 +1,7 @@
 function(CreateCompileGroup GROUP_NAME LIBRARY_NAME REQUIRED_DEPENDENCIES OPTIONAL_DEPENDENCIES)
 
+  option(MUQ_GROUP_${GROUP_NAME} "Should the group ${GROUP_NAME} be compiled?" ON)
+  
   set(MUQ_GROUPS "${MUQ_GROUPS};${GROUP_NAME}" CACHE INTERNAL "A list of all of the muq groups.")
   
   set(${GROUP_NAME}_REQUIRES ${REQUIRED_DEPENDENCIES} CACHE INTERNAL "External packages required by the ${GROUP_NAME} compile group.")
