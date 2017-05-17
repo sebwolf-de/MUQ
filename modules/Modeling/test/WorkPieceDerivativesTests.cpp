b7737a8db4442390d52c8a43a3c176ce0d72cf62
Andrew <andy@Andrews-MacBook-Pro.local>
2017-05-16 21:23:03 -0400

    set up linear function to test finite differences

--- modules/Modeling/test/WorkPieceDerivativesTests.cpp
+++ modules/Modeling/test/WorkPieceDerivativesTests.cpp
@@ -6,7 +6,43 @@
 
 using namespace muq::Modeling;
 
-/// A polynomial function to test the derivatives
+/// A linear function to test the finite difference derivatives
+class Linear : public WorkPiece {
+public:
+
+  Linear(Eigen::MatrixXd const& Q, Eigen::VectorXd const& b) :
+    WorkPiece(std::vector<std::string>({typeid(double).name(), typeid(Eigen::VectorXd).name()}), std::vector<std::string>({typeid(std::string).name(), typeid(Eigen::VectorXd).name()})),
+    Q(Q), b(b)
+  {}
+
+  virtual ~Linear() {}
+
+private:
+
+  virtual void EvaluateImpl(ref_vector<boost::any> const& inputs) override {
+    outputs.resize(2);
+
+    // a double to scale the linear part
+    const double a = boost::any_cast<double>(inputs[0]);
+
+    // constant reference to the input vector
+    const Eigen::VectorXd& in = boost::any_cast<Eigen::VectorXd>(inputs[1]);
+
+    // the first output is a string
+    outputs[0] = (std::string)"string";
+
+    // compute the linear function
+    outputs[1] = (Eigen::VectorXd)(Q*in + b);
+  }
+
+  /// Matrix for the linear part
+  const Eigen::MatrixXd Q;
+
+  /// Vector for the constant part
+  const Eigen::VectorXd b;
+};
+
+/// A quadratic function to test the derivatives
 class Quadratic : public WorkPiece {
 public:
 
@@ -24,7 +60,6 @@ private:
 
     // constant reference to the input vector
     const Eigen::VectorXd& in = boost::any_cast<Eigen::VectorXd>(inputs[0]);
-
     // compute the quadratic function
     outputs[0] = (in.transpose()*Q*in + a.transpose()*in + b) (0);
   }
@@ -84,8 +119,11 @@ public:
     // a random vector (for the constant term)
     b = Eigen::VectorXd::Random(1);
 
+    // create a linear polynomial
+    lin = std::make_shared<Linear>(Q, a);
+
     // create a quadratic polynomial
-    poly = std::make_shared<Quadratic>(Q, a, b);
+    quad = std::make_shared<Quadratic>(Q, a, b);
   }
 
   /// Default destructor
@@ -103,25 +141,56 @@ public:
   /// Vector for the constant part
   Eigen::VectorXd b;  
 
-  /// A polynomial function
-  std::shared_ptr<Quadratic> poly;
+  /// A quadratic function
+  std::shared_ptr<Quadratic> quad;
+
+  /// A linear function
+  std::shared_ptr<Linear> lin;
+
 private:
 };
 
+TEST_F(WorkPieceDerivativesTests, LinearFunction) {
+  // check input/output sizes
+  EXPECT_EQ(lin->numInputs, 2);
+  EXPECT_EQ(lin->numOutputs, 2);
+
+  // choose random inputs
+  const Eigen::VectorXd in = Eigen::VectorXd::Random(N);
+  const double scalar = 3.5;
+
+  // evaluate a
+  auto result = lin->Evaluate(scalar, in);
+
+  // make sure we get the expected result
+  EXPECT_EQ(result.size(), 2);
+  EXPECT_EQ(boost::any_cast<std::string>(result[0]).compare("string"), 0);
+  const Eigen::VectorXd& vec = boost::any_cast<Eigen::VectorXd>(result[1]);
+
+  // the expected vector
+  const Eigen::VectorXd expectedVec = Q*in+a;
+  
+  EXPECT_EQ(vec.size(), N);
+  EXPECT_EQ(expectedVec.size(), N);
+  for( unsigned int i=0; i<N; ++i ) {
+    EXPECT_DOUBLE_EQ(vec(i), expectedVec(i));
+  }
+}
+
 TEST_F(WorkPieceDerivativesTests, QuadraticFunction) {
   // check input/output sizes
-  EXPECT_EQ(poly->numInputs, 1);
-  EXPECT_EQ(poly->numOutputs, 1);
+  EXPECT_EQ(quad->numInputs, 1);
+  EXPECT_EQ(quad->numOutputs, 1);
 
   // choose random inputs
   const Eigen::VectorXd in = Eigen::VectorXd::Random(N);
 
   // evaluate and make sure we get the expected result
-  auto result = poly->Evaluate(in);
+  auto result = quad->Evaluate(in);
   EXPECT_DOUBLE_EQ(boost::any_cast<double>(result[0]), (in.transpose()*Q*in + a.transpose()*in + b) (0));
 
   // evaluate the jacobian 
-  auto jacBoost = poly->Jacobian(0, 0, in);
+  auto jacBoost = quad->Jacobian(0, 0, in);
   const Eigen::MatrixXd& jac = boost::any_cast<Eigen::MatrixXd>(jacBoost);
 
   // compute the expected jacobian
@@ -140,7 +209,7 @@ TEST_F(WorkPieceDerivativesTests, QuadraticFunction) {
   const Eigen::VectorXd vec0 = Eigen::VectorXd::Random(N);
 
   // evaluate the jacobian action
-  auto jacAction = poly->JacobianAction(0, 0, vec0, in);
+  auto jacAction = quad->JacobianAction(0, 0, vec0, in);
 
   // compute the expected jacobian action
   const double jacActionExpected = (2.0*in.transpose()*Q*vec0 + a.transpose()*vec0) (0);
@@ -152,7 +221,7 @@ TEST_F(WorkPieceDerivativesTests, QuadraticFunction) {
   const Eigen::VectorXd vec1 = Eigen::VectorXd::Random(1);
 
   // evaluate the jacobian action
-  auto jacTransposeActionBoost = poly->JacobianTransposeAction(0, 0, vec1, in);
+  auto jacTransposeActionBoost = quad->JacobianTransposeAction(0, 0, vec1, in);
   const Eigen::VectorXd& jacTransposeAction = boost::any_cast<Eigen::VectorXd>(jacTransposeActionBoost);
 
   // compute the expected jacobian action
