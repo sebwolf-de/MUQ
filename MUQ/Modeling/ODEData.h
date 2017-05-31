ee977f591629bd5de73263a6d2cb7944cd45bd90
Andrew <andy@Andrews-MacBook-Pro.local>
2017-05-31 01:35:47 -0400

    cleaned up ODEData

--- MUQ/Modeling/ODEData.h
+++ MUQ/Modeling/ODEData.h
@@ -5,21 +5,43 @@
 
 namespace muq {
   namespace Modeling {
+    /// A helper class for Sundial's time integration
+    /**
+       Stores necessary muq::Modeling::WorkPiece's and thier inputs so Sundials can hold that information.
+     */
     class ODEData {
     public:
 
+      /// Construct basic ode data
+      /**
+	 @param[in] rhs A muq::Modeling::WorkPiece that evalautes the right hand side of an ODE
+	 @param[in] inputs The inputs to the rhs --- the first is the state, the rest are constant in time
+	 @param[in] wrtIn The input we are computing the derivative wrt --- negative indicates no derivative is being computed
+	 @param[in] wrtOut The output we are computing the derivative of --- negative indicates no derivative is being computed
+       */
       ODEData(std::shared_ptr<WorkPiece> rhs, ref_vector<boost::any> const& inputs, int const wrtIn, int const wrtOut);
 
+      /// Construct with root function
+      /**
+	 @param[in] rhs A muq::Modeling::WorkPiece that evalautes the right hand side of an ODE
+	 @param[in] root A muq::Modeling::WorkPiece that evalautes a function we are trying to find the root of along an orbit of the ODE
+	 @param[in] inputs The inputs to the rhs --- the first is the state, the rest are constant in time
+       */
       ODEData(std::shared_ptr<WorkPiece> rhs, std::shared_ptr<WorkPiece> root, ref_vector<boost::any> const& inputs);
 
+      /// The right hand side of the ODE
       std::shared_ptr<WorkPiece> rhs;
 
+      /// A function we are trying to find the root of along an orbit of the ODE (nullptr if we are not doing a root finding problem)
       std::shared_ptr<WorkPiece> root;
 
+      /// The inputs to the rhs --- the first is the state, the rest are constant in time
       ref_vector<boost::any> inputs;
 
+      /// The input we are computing the derivative wrt --- negative indicates no derivative is being computed
       int wrtIn = -1;
 
+      /// The output we are computing the derivative of --- negative indicates no derivative is being computed
       int wrtOut = -1;
       
     private:
--- modules/Modeling/src/ODEData.cpp
+++ modules/Modeling/src/ODEData.cpp
@@ -4,7 +4,9 @@
 
 using namespace muq::Modeling;
 
+// construct basic ode data
 ODEData::ODEData(std::shared_ptr<WorkPiece> rhs, ref_vector<boost::any> const& inputs, int const wrtIn, int const wrtOut) : rhs(rhs), inputs(inputs), wrtIn(wrtIn), wrtOut(wrtOut) {}
 
+// construct with root function
 ODEData::ODEData(std::shared_ptr<WorkPiece> rhs, std::shared_ptr<WorkPiece> root, ref_vector<boost::any> const& inputs) : rhs(rhs), root(root), inputs(inputs) {}
 
