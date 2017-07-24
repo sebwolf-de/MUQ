d271f432364c54e683661c48a4a572badeb54831
Matthew Parno <Matthew.D.Parno.civ@mail.mil>
2017-07-24 08:46:10 -0400

    Added special companion matrix operator.

--- modules/Utilities/test/LinearAlgebra/LinearOperatorTests.cpp
+++ modules/Utilities/test/LinearAlgebra/LinearOperatorTests.cpp
@@ -1,5 +1,6 @@
 #include "MUQ/Utilities/LinearAlgebra/LinearOperator.h"
 #include "MUQ/Utilities/LinearAlgebra/EigenLinearOperator.h"
+#include "MUQ/Utilities/LinearAlgebra/CompanionMatrix.h"
 
 #include <random>
 
@@ -95,5 +96,48 @@ TEST(Utilities_LinearOperator, Conversion)
 {
     Eigen::MatrixXd A(10,4);
     std::shared_ptr<LinearOperator> Aop = LinearOperator::Create(A);
+
+}
+
+
+TEST(Utilities_LinearOperator, CompanionMatrix)
+{
+
+    Eigen::VectorXd lastRow(3);
+    lastRow << 1.0, 1.5, 1.75;
+
+    CompanionMatrix A(lastRow);
+    
+    Eigen::MatrixXd x = Eigen::MatrixXd::Random(3,10);
+
+    // Test the multiplication
+    Eigen::MatrixXd b = A.Apply(x);
+
+    Eigen::VectorXd trueRow = lastRow.transpose()*x;
+        
+    for(int j=0; j<x.cols(); ++j)
+    {
+        for(int i=0; i<b.rows()-1; ++i)
+        {
+            EXPECT_DOUBLE_EQ(x(i+1,j), b(i,j));
+        }
+
+        EXPECT_DOUBLE_EQ(trueRow(j), b(b.rows()-1,j));
+    }
+
+    // Test the transpose application
+    b = A.ApplyTranspose(x);
+    Eigen::MatrixXd trueTrans = Eigen::MatrixXd::Zero(3,3);
+    trueTrans.block(1,0,2,2) = Eigen::MatrixXd::Identity(2,2);
+    trueTrans.col(2) = lastRow;
+    Eigen::MatrixXd trueB = trueTrans*x;
+    
+    for(int j=0; j<b.cols(); ++j)
+    {
+        for(int i=0; i<b.rows(); ++i)
+        {
+            EXPECT_DOUBLE_EQ(trueB(i,j), b(i,j));
+        }
+    }
 
 }
