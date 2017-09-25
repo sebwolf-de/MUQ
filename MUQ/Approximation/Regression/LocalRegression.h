12a75ec260cecfa2733a7165c62b5668628b6550
Andrew <andy@Andrews-MacBook-Pro.local>
2017-09-25 10:10:39 -0400

    added framework for local regression

---
+++ MUQ/Approximation/Regression/LocalRegression.h
@@ -0,0 +1,19 @@
+#ifndef LOCALREGRESSION_H_
+#define LOCALREGRESSION_H_
+
+namespace muq {
+  namespace Approximation {
+    class LocalRegression {
+    public:
+
+      LocalRegression();
+
+      ~LocalRegression();
+      
+    private:
+    };
+  } // namespace Approximation
+} // namespace muq
+
+
+#endif
---
+++ modules/Approximation/src/Regression/LocalRegression.cpp
@@ -0,0 +1,7 @@
+#include "MUQ/Approximation/Regression/LocalRegression.h"
+
+using namespace muq::Approximation;
+
+LocalRegression::LocalRegression() {}
+
+LocalRegression::~LocalRegression() {}
