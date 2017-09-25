131e93099f1f8767e84ded1382ca0fe5f48f84d0
Andrew <andy@Andrews-MacBook-Pro.local>
2017-09-25 10:12:30 -0400

    added WorkPiece dependency

--- MUQ/Approximation/Regression/LocalRegression.h
+++ MUQ/Approximation/Regression/LocalRegression.h
@@ -1,9 +1,11 @@
 #ifndef LOCALREGRESSION_H_
 #define LOCALREGRESSION_H_
 
+#include "MUQ/Modeling/WorkPiece.h"
+
 namespace muq {
   namespace Approximation {
-    class LocalRegression {
+    class LocalRegression : public muq::Modeling::WorkPiece {
     public:
 
       LocalRegression();
@@ -11,6 +13,9 @@ namespace muq {
       ~LocalRegression();
       
     private:
+
+      virtual void EvaluateImpl(muq::Modeling::ref_vector<boost::any> const& inputs) override;
+      
     };
   } // namespace Approximation
 } // namespace muq
--- modules/Approximation/src/Regression/LocalRegression.cpp
+++ modules/Approximation/src/Regression/LocalRegression.cpp
@@ -1,7 +1,12 @@
 #include "MUQ/Approximation/Regression/LocalRegression.h"
 
+using namespace muq::Modeling;
 using namespace muq::Approximation;
 
 LocalRegression::LocalRegression() {}
 
 LocalRegression::~LocalRegression() {}
+
+void LocalRegression::EvaluateImpl(ref_vector<boost::any> const& inputs) {
+  
+}
