00a8bf81979770ddc325fe16d5c579dfb08b8c03
Andrew Davis <andy@Andrews-MacBook-Pro-2.local>
2017-11-10 17:43:59 -0500

    added Monte Carlo framework

---
+++ MUQ/SamplingAlgorithms/MonteCarlo.h
@@ -0,0 +1,18 @@
+#ifndef MONTECARLO_H_
+#define MONTECARLO_H_
+
+#include "MUQ/SamplingAlgorithms/SamplingAlgorithm.h"
+
+namespace muq {
+  namespace SamplingAlgorithms {
+    class MonteCarlo : public SamplingAlgorithm {
+    public:
+
+      MonteCarlo();
+      
+    private:
+    };
+  } // namespace SamplingAlgorithms
+} // namespace muq
+
+#endif
---
+++ modules/SamplingAlgorithms/src/MonteCarlo.cpp
@@ -0,0 +1,5 @@
+#include "MUQ/SamplingAlgorithms/MonteCarlo.h"
+
+using namespace muq::SamplingAlgorithms;
+
+MonteCarlo::MonteCarlo() : SamplingAlgorithm() {}
