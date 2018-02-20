93b1616a7ff4fef96035b54bb08b168ad1e5b5f1
Matthew Parno <Matthew.D.Parno@erdc.dren.mil>
2018-02-20 16:34:21 -0500

    Fixed capitalization in BuildBoost.cmake

--- cmake/builds/BuildBoost.cmake
+++ cmake/builds/BuildBoost.cmake
@@ -2,31 +2,31 @@ include(ExternalProject)
 
 if(NOT DEFINED BOOST_EXTERNAL_SOURCE)
   #NOTE: when updated to 1.63.0, you can remove the serialization patch below.
-  set(BOOST_EXTERNAL_SOURCE http://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.gz)	
+  set(BOOST_EXTERNAL_SOURCE http://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.gz)
 endif()
 
 
 
-set(BOOST_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/external/boost/src/Boost")
-  
+set(BOOST_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/external/boost/src/BOOST")
+
 # decide what toolset boost should use, start with compiler types, then work through operating systems
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
-  
+
   # using Intel C++
   if(WIN32)
     set(BOOST_TOOLSET_NAME intel-win32)
   else()
     set(BOOST_TOOLSET_NAME intel-linux)
   endif()
-  
+
   set(BOOST_CXX_FLAGS "-std=c++11")
   set(BOOST_LINK_FLAGS "")
-  
+
 # is this an OSX machine?
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 
   set(BOOST_TOOLSET_NAME darwin)
-  
+
   if(MUQ_USE_LIBC11)
   	set(BOOST_CXX_FLAGS "-std=c++11 -stdlib=libc++")
   	set(BOOST_LINK_FLAGS "-stdlib=libc++")
@@ -34,12 +34,12 @@ elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 	set(BOOST_CXX_FLAGS "-std=c++11")
 	set(BOOST_LINK_FLAGS "")
   endif(MUQ_USE_LIBC11)
-  
+
 # is the compiler clang?
 elseif("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
-    
+
     set(BOOST_TOOLSET_NAME clang)
-  
+
     if(MUQ_USE_LIBC11)
     	set(BOOST_CXX_FLAGS "-std=c++11 -stdlib=libc++")
     	set(BOOST_LINK_FLAGS "-stdlib=libc++")
@@ -47,18 +47,18 @@ elseif("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
         set(BOOST_CXX_FLAGS "-std=c++11")
         set(BOOST_LINK_FLAGS "")
     endif(MUQ_USE_LIBC11)
-	
+
 # is the compiler g++?
 elseif(CMAKE_COMPILER_IS_GNUCXX)
-        
+
     set(BOOST_TOOLSET_NAME gcc)
-        
+
     set(BOOST_CXX_FLAGS "-std=c++11")
     set(BOOST_LINK_FLAGS "")
-	
+
 # is this an windows machine?
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-  
+
   if(MINGW)
     #using MINGW
     set(BOOST_TOOLSET_NAME mingw)
@@ -68,10 +68,10 @@ elseif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
   else()
     message( FATAL_ERROR "Unable to find a BOOST toolset that matches your compiler and system.  Either use a different compiler, or try installing boost manually." )
   endif()
-  
+
   set(BOOST_CXX_FLAGS "-std=c++11")
   set(BOOST_LINK_FLAGS "")
-  
+
 else()
         message( FATAL_ERROR "Unable to find a BOOST toolset that matches your compiler and system.  Either use a different compiler, or try installing boost manually." )
 endif()
@@ -91,15 +91,15 @@ endif()
 message(STATUS "BOOST_LINK_FLAGS = ${BOOST_LINK_FLAGS}")
 message(STATUS "BOOST_CXX_FLAGS = ${BOOST_CXX_FLAGS}")
 
-        
+
 if(MUQ_USE_OPENMPI)
     if(MUQ_USE_LIBC11)
         ExternalProject_Add(
             BOOST
             PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/boost
             URL ${BOOST_EXTERNAL_SOURCE}
-            PATCH_COMMAND cp ${CMAKE_SOURCE_DIR}/external/boost/shared_ptr_helper.hpp ${CMAKE_CURRENT_BINARY_DIR}/external/boost/src/Boost/boost/serialization/shared_ptr_helper.hpp
-            UPDATE_COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/user-config.jam ${BOOST_BUILD_DIR}/user-config.jam 
+            PATCH_COMMAND cp ${CMAKE_SOURCE_DIR}/external/boost/shared_ptr_helper.hpp ${CMAKE_CURRENT_BINARY_DIR}/external/boost/src/BOOST/boost/serialization/shared_ptr_helper.hpp
+            UPDATE_COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/user-config.jam ${BOOST_BUILD_DIR}/user-config.jam
             CONFIGURE_COMMAND ${BOOST_BUILD_DIR}/bootstrap.sh --prefix=${Boost_INSTALL_DIR}
             BUILD_COMMAND ${BOOST_BUILD_DIR}/b2 cxxflags=${BOOST_CXX_FLAGS} linkflags=${BOOST_LINK_FLAGS} variant=release --user-config=${BOOST_BUILD_DIR}/user-config.jam toolset=${BOOST_TOOLSET_NAME}-muq  --with-filesystem --with-graph --with-serialization --with-system --with-mpi install
             BUILD_IN_SOURCE 1
@@ -110,9 +110,9 @@ if(MUQ_USE_OPENMPI)
               BOOST
               PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/boost
               URL ${BOOST_EXTERNAL_SOURCE}
-              PATCH_COMMAND cp ${CMAKE_SOURCE_DIR}/external/boost/shared_ptr_helper.hpp ${CMAKE_CURRENT_BINARY_DIR}/external/boost/src/Boost/boost/serialization/shared_ptr_helper.hpp
-              UPDATE_COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/user-config.jam ${BOOST_BUILD_DIR}/user-config.jam 
-              CONFIGURE_COMMAND ${BOOST_BUILD_DIR}/bootstrap.sh --prefix=${Boost_INSTALL_DIR} 
+              PATCH_COMMAND cp ${CMAKE_SOURCE_DIR}/external/boost/shared_ptr_helper.hpp ${CMAKE_CURRENT_BINARY_DIR}/external/boost/src/BOOST/boost/serialization/shared_ptr_helper.hpp
+              UPDATE_COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/user-config.jam ${BOOST_BUILD_DIR}/user-config.jam
+              CONFIGURE_COMMAND ${BOOST_BUILD_DIR}/bootstrap.sh --prefix=${Boost_INSTALL_DIR}
               BUILD_COMMAND ${BOOST_BUILD_DIR}/b2 cxxflags=${BOOST_CXX_FLAGS} variant=release --user-config=${BOOST_BUILD_DIR}/user-config.jam toolset=${BOOST_TOOLSET_NAME}-muq --with-filesystem --with-graph --with-serialization --with-system --with-mpi install
               BUILD_IN_SOURCE 1
               INSTALL_COMMAND ""
@@ -124,8 +124,8 @@ else(MUQ_USE_OPENMPI)
             BOOST
             PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/boost
             URL ${BOOST_EXTERNAL_SOURCE}
-            PATCH_COMMAND cp ${CMAKE_SOURCE_DIR}/external/boost/shared_ptr_helper.hpp ${CMAKE_CURRENT_BINARY_DIR}/external/boost/src/Boost/boost/serialization/shared_ptr_helper.hpp
-            UPDATE_COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/user-config.jam ${BOOST_BUILD_DIR}/user-config.jam 
+            PATCH_COMMAND cp ${CMAKE_SOURCE_DIR}/external/boost/shared_ptr_helper.hpp ${CMAKE_CURRENT_BINARY_DIR}/external/boost/src/BOOST/boost/serialization/shared_ptr_helper.hpp
+            UPDATE_COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/user-config.jam ${BOOST_BUILD_DIR}/user-config.jam
             CONFIGURE_COMMAND ${BOOST_BUILD_DIR}/bootstrap.sh --prefix=${Boost_INSTALL_DIR}
             BUILD_COMMAND ${BOOST_BUILD_DIR}/b2 cxxflags=${BOOST_CXX_FLAGS} linkflags=${BOOST_LINK_FLAGS} variant=release -d+2 --user-config=${BOOST_BUILD_DIR}/user-config.jam toolset=${BOOST_TOOLSET_NAME}-muq --with-filesystem --with-graph --with-serialization --with-system  install
             BUILD_IN_SOURCE 1
@@ -136,8 +136,8 @@ else(MUQ_USE_OPENMPI)
               BOOST
               PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/boost
               URL ${BOOST_EXTERNAL_SOURCE}
-              PATCH_COMMAND cp ${CMAKE_SOURCE_DIR}/external/boost/shared_ptr_helper.hpp ${CMAKE_CURRENT_BINARY_DIR}/external/boost/src/Boost/boost/serialization/shared_ptr_helper.hpp
-              UPDATE_COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/user-config.jam ${BOOST_BUILD_DIR}/user-config.jam 
+              PATCH_COMMAND cp ${CMAKE_SOURCE_DIR}/external/boost/shared_ptr_helper.hpp ${CMAKE_CURRENT_BINARY_DIR}/external/boost/src/BOOST/boost/serialization/shared_ptr_helper.hpp
+              UPDATE_COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/user-config.jam ${BOOST_BUILD_DIR}/user-config.jam
               CONFIGURE_COMMAND ${BOOST_BUILD_DIR}/bootstrap.sh --prefix=${Boost_INSTALL_DIR}
               BUILD_COMMAND ${BOOST_BUILD_DIR}/b2 cxxflags=${BOOST_CXX_FLAGS} variant=release -d+2 --user-config=${BOOST_BUILD_DIR}/user-config.jam toolset=${BOOST_TOOLSET_NAME}-muq --with-filesystem --with-graph --with-serialization --with-system  install
               BUILD_IN_SOURCE 1
@@ -145,7 +145,7 @@ else(MUQ_USE_OPENMPI)
               )
     endif(MUQ_USE_LIBC11)
 endif(MUQ_USE_OPENMPI)
-        
+
 set_property( TARGET BOOST PROPERTY FOLDER "Externals")
 
 set( Boost_INCLUDE_DIRS ${Boost_INSTALL_DIR}/include )
@@ -168,7 +168,3 @@ if(MUQ_USE_OPENMPI)
   list(APPEND Boost_LIBRARIES ${Boost_INSTALL_DIR}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}boost_mpi${CMAKE_SHARED_LIBRARY_SUFFIX})
   list(APPEND Boost_LIBRARIES_STATIC ${Boost_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}boost_mpi${CMAKE_STATIC_LIBRARY_SUFFIX} )
 endif(MUQ_USE_OPENMPI)
-
-
-
-  
