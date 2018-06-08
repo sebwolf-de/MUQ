f7d9d09ae5cc10e84634fece083c44624ad9e72f
Brendan West <brendan.a.west@erdc.dren.mil>
2018-06-08 15:29:44 -0400

    WIP - GaussianProcess wrapping. A couple more compile errors to work through.

--- modules/Approximation/python/KLWrapper.cpp
+++ modules/Approximation/python/KLWrapper.cpp
@@ -1,7 +1,6 @@
 #include "AllClassWrappers.h"
 
-#include ".h"
-#include "KarhunenLoeveFactory.h"
+#include "MUQ/Approximation/GaussianProcesses/KarhunenLoeveExpansion.h"
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -17,5 +16,14 @@ namespace py = pybind11;
 
 void muq::Approximation::PythonBindings::KLWrapper(py::module &m)
 {
-    
+  // KarhunenLoeveExpansion class  
+  py::class_<KarhunenLoeveExpansion, std::shared_ptr<KarhunenLoeveExpansion>> 
+    KLExpansion(m, "KarhunenLoeveExpansion");
+  KLExpansion
+    .def(py::init<std::shared_ptr<KernelBase>, Eigen::MatrixXd const&,
+                  Eigen::VectorXd const&>())
+    .def(py::init<std::shared_ptr<KernelBase>, Eigen::MatrixXd const&,
+                  Eigen::VectorXd const&, boost::property_tree::ptree>())
+    .def("GetModes", &KarhunenLoeveExpansion::GetModes)
+    .def("Evaluate", &KarhunenLoeveExpansion::Evaluate);
 }
