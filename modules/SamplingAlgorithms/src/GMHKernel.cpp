1db5a2b0f2b65fa3de3de260055c378da148ea34
davisad <andrew.add@gmail.com>
2018-08-14 14:45:41 -0400

    implemented framework for GMH kernel

--- modules/SamplingAlgorithms/src/GMHKernel.cpp
+++ modules/SamplingAlgorithms/src/GMHKernel.cpp
@@ -2,36 +2,89 @@
 
 #include <parcer/Queue.h>
 
+#include <Eigen/Eigenvalues>
+
+#include "MUQ/Utilities/RandomGenerator.h"
+
 namespace pt = boost::property_tree;
+using namespace muq::Utilities;
 using namespace muq::SamplingAlgorithms;
 
 REGISTER_TRANSITION_KERNEL(GMHKernel)
 
-GMHKernel::GMHKernel(pt::ptree const& pt, std::shared_ptr<AbstractSamplingProblem> problem) : MHKernel(pt, problem), N(pt.get<unsigned int>("NumProposals")) {}
+GMHKernel::GMHKernel(pt::ptree const& pt, std::shared_ptr<AbstractSamplingProblem> problem) : MHKernel(pt, problem),
+  N(pt.get<unsigned int>("NumProposals")),
+  Np1(N+1),
+  M(pt.get<unsigned int>("NumAccpeted", N)) {}
 
-GMHKernel::GMHKernel(pt::ptree const& pt, std::shared_ptr<AbstractSamplingProblem> problem, std::shared_ptr<MCMCProposal> proposalIn) : MHKernel(pt, problem, proposalIn), N(pt.get<unsigned int>("NumProposals")) {}
+GMHKernel::GMHKernel(pt::ptree const& pt, std::shared_ptr<AbstractSamplingProblem> problem, std::shared_ptr<MCMCProposal> proposalIn) :
+  MHKernel(pt, problem, proposalIn),
+  N(pt.get<unsigned int>("NumProposals")),
+  Np1(N+1),
+  M(pt.get<unsigned int>("NumAccpeted", N)) {}
 
 GMHKernel::~GMHKernel() {}
 
 void GMHKernel::PreStep(unsigned int const t, std::shared_ptr<SamplingState> state) {
   // propose N steps
-  std::cout << "propose N steps" << std::endl;
-  std::vector<std::shared_ptr<SamplingState> > proposals(N+1);
-  proposals[0] = state;
-  for( auto it = proposals.begin()+1; it!=proposals.end(); ++it ) {
+  proposedStates.resize(Np1, nullptr);
+  proposedStates[0] = state;
+  for( auto it = proposedStates.begin()+1; it!=proposedStates.end(); ++it ) {
     *it = proposal->Sample(state); 
   }
 
   // compute log-target
-  std::cout << "compute log-target" << std::endl;
-  for( auto it : proposals ) { std::cout << problem->LogDensity(it) << std::endl; }
+  Eigen::VectorXd R = Eigen::VectorXd::Zero(Np1);
+  for( unsigned int i=0; i<Np1; ++i ) {
+    R(i) = problem->LogDensity(proposedStates[i]);
+    for( auto k : proposedStates ) {
+      if( k==proposedStates[i] ) { continue; }
+      R(i) += proposal->LogDensity(proposedStates[i], k);
+    }
+  }
 
   // compute stationary acceptance transition probability
-  std::cout << "compute acceptance ratio" << std::endl;
+  Eigen::MatrixXd A = Eigen::MatrixXd::Ones(Np1,Np1);
+  for( unsigned int i=0; i<Np1; ++i ) {
+    for( unsigned int j=0; j<Np1; ++j ) {
+      if( j==i ) { continue; }
+      A(i,j) = std::fmin(1.0, R(j)/R(i))/(double)(Np1);
+      A(i,i) -= A(i,j);
+    }
+  }
 
+  Eigen::EigenSolver<Eigen::MatrixXd> eig(A);
+  // the larget eigenvalue should be 1
+  assert(std::fabs(eig.eigenvalues() (0).real()-1.0)<1.0e-10);
+  assert(std::fabs(eig.eigenvalues() (0).imag())<1.0e-10);
+
+  stationaryAcceptance = eig.eigenvectors().col(0).real();
+  stationaryAcceptance /= stationaryAcceptance.sum();
+  
+  // compute the cumulative sum
+  for( unsigned int i=1; i<Np1; ++i ) { stationaryAcceptance(i) += stationaryAcceptance(i-1); }
 }
 
-/*std::vector<std::shared_ptr<SamplingState> > GMHKernel::Step(unsigned int const t, std::shared_ptr<SamplingState> state) {
-  return std::vector<std::shared_ptr<SamplingState> >(1, nullptr);
-  }*/
+std::vector<std::shared_ptr<SamplingState> > GMHKernel::Step(unsigned int const t, std::shared_ptr<SamplingState> state) {
+  std::vector<std::shared_ptr<SamplingState> > newStates(M, nullptr);
 
+  // sample the new states
+  for( auto it=newStates.begin(); it!=newStates.end(); ++it ) {
+    // determine which proposed state to return
+    const double uniform = RandomGenerator::GetUniform();
+    unsigned int index = 0;
+    for( ; index<Np1; ++index ) { if( uniform<stationaryAcceptance(index) ) { break; }  }
+
+    // store it
+    *it = proposedStates[index];
+  }
+  
+  return newStates;
+}
+
+Eigen::VectorXd GMHKernel::CumulativeStationaryAcceptance() const {
+  // make sure this object has been populated
+  assert(stationaryAcceptance.size()==Np1);
+  
+  return stationaryAcceptance;
+}
--- modules/SamplingAlgorithms/test/GMHKernelTests.cpp
+++ modules/SamplingAlgorithms/test/GMHKernelTests.cpp
@@ -18,6 +18,9 @@ TEST(GMHKernelTest, PrposalTest) {
   // create a sampling problem
   auto problem = std::make_shared<SamplingProblem>(dist);
 
+  const unsigned int N = 10;
+  const unsigned int M = 10;
+
   // use an apative Metropolis proposal
   pt::ptree pt;
   pt.put("MyKernel.MyProposal.ProposalVariance", 1.0);
@@ -27,7 +30,8 @@ TEST(GMHKernelTest, PrposalTest) {
   pt.put("MyKernel.MyProposal.AdaptScale", 1.0);
   pt.put("MyKernel.MyProposal.Method", "AMProposal");
   pt.put("MyKernel.Proposal", "MyProposal");
-  pt.put("MyKernel.NumProposals", 10);
+  pt.put("MyKernel.NumProposals", N);
+  pt.put("MyKernel.NumAccepted", M); // optional: defaults to N
 
   // create the kernel
   auto kern = std::make_shared<GMHKernel>(pt.get_child("MyKernel"), problem);
@@ -37,4 +41,12 @@ TEST(GMHKernelTest, PrposalTest) {
 
   // propose a bunch of points in the pre step
   kern->PreStep(0, state);
+  const Eigen::VectorXd& cumulativeStationaryAcceptance = kern->CumulativeStationaryAcceptance();
+  EXPECT_EQ(cumulativeStationaryAcceptance.size(), N+1);
+  EXPECT_DOUBLE_EQ(cumulativeStationaryAcceptance(N), 1.0);
+  for( unsigned int i=0; i<N+1; ++i ) { EXPECT_TRUE(cumulativeStationaryAcceptance(i)>0.0); }
+
+  // accept/reject M of the proposed states
+  std::vector<std::shared_ptr<SamplingState> > newStates = kern->Step(0, state);
+  EXPECT_EQ(newStates.size(), M);
 }
