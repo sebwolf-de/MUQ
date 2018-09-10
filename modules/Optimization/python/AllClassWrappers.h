2e990b44b7b9a76e7395a11c7a10f01ef66fe7de
Andrew Davis <davisad@alum.mit.edu>
2018-09-10 08:45:52 -0400

    created framework for optimization python interface

---
+++ modules/Optimization/python/AllClassWrappers.h
@@ -0,0 +1,14 @@
+#ifndef OPTIMIZATION_ALLCLASSWRAPPERS_H_
+#define OPTIMIZATION_ALLCLASSWRAPPERS_H_
+
+#include <pybind11/pybind11.h>
+
+namespace muq {
+  namespace Optimization {
+    namespace PythonBindings {
+    } // namespace PythonBindings
+  } // namespace Optimization
+} // namespace muq
+
+
+#endif
