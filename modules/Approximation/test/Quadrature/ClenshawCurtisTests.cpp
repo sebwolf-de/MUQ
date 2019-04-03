82510422a4ab5acbf1d5d843b9c52edd6074baef
Matthew Parno <Matthew.D.Parno@erdc.dren.mil>
2019-04-03 13:09:38 -0400

    Fixed clenshaw curtis test.

--- modules/Approximation/test/Quadrature/ClenshawCurtisTests.cpp
+++ modules/Approximation/test/Quadrature/ClenshawCurtisTests.cpp
@@ -7,7 +7,7 @@ using namespace muq::Approximation;
 TEST(Quadrature, ClenshawCurtis) {
 
   // polynomial order
-  int order = 4;
+  int order = 2;
 
   // legendre table for polynomial order n=5
   std::vector<double> ptsTable = {-1.0000000000000000,
@@ -29,8 +29,10 @@ TEST(Quadrature, ClenshawCurtis) {
   quad.Compute(order);
   Eigen::VectorXd pts = quad.Points().transpose();
   Eigen::VectorXd wts = quad.Weights();
+  EXPECT_EQ(ptsTable.size(), pts.size());
+  EXPECT_EQ(wtsTable.size(), wts.size());
 
-  for (int i=0; i<order+1; i++) {
+  for (int i=0; i<std::min<int>(wts.size(),wtsTable.size()); i++) {
     EXPECT_NEAR(pts(i), ptsTable[i], 1e-9);
     EXPECT_NEAR(wts(i), wtsTable[i], 1e-9);
   }
