94074f917a90065f5f09c0d48c217a99a46c17d6
Matthew Parno <Matthew.D.Parno@erdc.dren.mil>
2019-04-09 14:21:35 -0400

    Added Gauss-Patterson and exponential growth quadrature rules.

--- modules/Approximation/test/Quadrature/AdaptiveSmolyakQuadratureTest.cpp
+++ modules/Approximation/test/Quadrature/AdaptiveSmolyakQuadratureTest.cpp
@@ -4,6 +4,8 @@
 #include "MUQ/Approximation/Quadrature/SmolyakQuadrature.h"
 #include "MUQ/Approximation/Quadrature/GaussQuadrature.h"
 #include "MUQ/Approximation/Quadrature/ClenshawCurtisQuadrature.h"
+#include "MUQ/Approximation/Quadrature/GaussPattersonQuadrature.h"
+#include "MUQ/Approximation/Quadrature/ExponentialGrowthQuadrature.h"
 
 #include "MUQ/Approximation/Polynomials/Legendre.h"
 
@@ -78,7 +80,6 @@ TEST(Quadrature, AdaptiveSmolyak_AdaptiveClenshawCurtis) {
   auto model = std::make_shared<CosOperator>(dim);
 
   // Define the quadrature rules
-  auto poly1d = std::make_shared<Legendre>();
   auto quad1d = std::make_shared<ClenshawCurtisQuadrature>();
 
   boost::property_tree::ptree options;
@@ -92,6 +93,63 @@ TEST(Quadrature, AdaptiveSmolyak_AdaptiveClenshawCurtis) {
 
   Eigen::VectorXd res = smolyQuad.Compute(multiSet, options);
 
+  EXPECT_NEAR(4.0*std::sin(1.0), res(0), 1e-10);
+  EXPECT_NEAR(4.0*std::sin(1.0), res(1), 1e-10);
+
+  EXPECT_LT(smolyQuad.Error(), 1e-12);
+}
+
+
+TEST(Quadrature, AdaptiveSmolyak_AdaptiveGaussPatterson) {
+
+  unsigned int dim = 2;
+
+  // Define the model
+  auto model = std::make_shared<CosOperator>(dim);
+
+  // Define the quadrature rules
+  auto quad1d = std::make_shared<GaussPattersonQuadrature>();
+
+  boost::property_tree::ptree options;
+  options.put("ShouldAdapt",true);
+  options.put("ErrorTol",1e-12);
+
+  AdaptiveSmolyakQuadrature smolyQuad(model, {quad1d, quad1d});
+
+  unsigned int maxOrder = 1;
+  auto multiSet = MultiIndexFactory::CreateTotalOrder(dim, maxOrder);
+
+  Eigen::VectorXd res = smolyQuad.Compute(multiSet, options);
+
+  EXPECT_NEAR(4.0*std::sin(1.0), res(0), 1e-10);
+  EXPECT_NEAR(4.0*std::sin(1.0), res(1), 1e-10);
+
+  EXPECT_LT(smolyQuad.Error(), 1e-12);
+}
+
+TEST(Quadrature, AdaptiveSmolyak_AdaptiveExponential) {
+
+  unsigned int dim = 2;
+
+  // Define the model
+  auto model = std::make_shared<CosOperator>(dim);
+
+  // Define the quadrature rules
+  auto poly1d = std::make_shared<Legendre>();
+  auto baseQuad = std::make_shared<GaussQuadrature>(poly1d);
+  auto quad1d = std::make_shared<ExponentialGrowthQuadrature>(baseQuad);
+
+  boost::property_tree::ptree options;
+  options.put("ShouldAdapt",true);
+  options.put("ErrorTol",1e-12);
+
+  AdaptiveSmolyakQuadrature smolyQuad(model, {quad1d, quad1d});
+
+  unsigned int maxOrder = 1;
+  auto multiSet = MultiIndexFactory::CreateTotalOrder(dim, maxOrder);
+
+  Eigen::VectorXd res = smolyQuad.Compute(multiSet, options);
+  
   EXPECT_NEAR(4.0*std::sin(1.0), res(0), 1e-10);
   EXPECT_NEAR(4.0*std::sin(1.0), res(1), 1e-10);
 
