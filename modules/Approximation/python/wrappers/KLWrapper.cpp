dcffc4f1afaeadd01139a160dea3b46038f1c627
Andrew Davis <davisad@alum.mit.edu>
2019-07-22 10:23:52 -0400

    python dict to ptree bug fix

--- modules/Approximation/python/wrappers/KLWrapper.cpp
+++ modules/Approximation/python/wrappers/KLWrapper.cpp
@@ -11,20 +11,23 @@
 #include <functional>
 #include <vector>
 
+#include "MUQ/Utilities/PyDictConversion.h"
+
+using namespace muq::Utilities;
 using namespace muq::Approximation::PythonBindings;
 using namespace muq::Approximation;
 namespace py = pybind11;
 
 void muq::Approximation::PythonBindings::KLWrapper(py::module &m)
 {
-  // KarhunenLoeveExpansion class  
-  py::class_<KarhunenLoeveExpansion, std::shared_ptr<KarhunenLoeveExpansion>> 
+  // KarhunenLoeveExpansion class
+  py::class_<KarhunenLoeveExpansion, std::shared_ptr<KarhunenLoeveExpansion>>
     KLExpansion(m, "KarhunenLoeveExpansion");
   KLExpansion
     .def(py::init<std::shared_ptr<KernelBase>, Eigen::MatrixXd const&,
                   Eigen::VectorXd const&>())
-    .def(py::init<std::shared_ptr<KernelBase>, Eigen::MatrixXd const&,
-                  Eigen::VectorXd const&, boost::property_tree::ptree>())
+    .def(py::init( [](std::shared_ptr<KernelBase> const& kern, Eigen::MatrixXd const& pts,
+                  Eigen::VectorXd const& wts, py::dict const& d) { return new KarhunenLoeveExpansion(kern, pts, wts, ConvertDictToPtree(d)); } ))
     .def("GetModes", &KarhunenLoeveExpansion::GetModes)
     .def("Evaluate", &KarhunenLoeveExpansion::Evaluate);
 }
