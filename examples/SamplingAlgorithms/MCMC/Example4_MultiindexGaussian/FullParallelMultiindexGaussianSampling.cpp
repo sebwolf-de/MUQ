4d144fc005ce1c20b769fae7cda602ef3f51be67
Linus Seelinger <S.Linus@gmx.de>
2020-03-19 14:01:53 +0100

    Add full parallel MIMCMC example

---
+++ examples/SamplingAlgorithms/MCMC/Example4_MultiindexGaussian/FullParallelMultiindexGaussianSampling.cpp
@@ -0,0 +1,56 @@
+#include "MUQ/SamplingAlgorithms/SLMCMC.h"
+#include "MUQ/SamplingAlgorithms/GreedyMLMCMC.h"
+#include "MUQ/SamplingAlgorithms/MIMCMC.h"
+
+#include "MUQ/Modeling/Distributions/Gaussian.h"
+#include "MUQ/Modeling/Distributions/Density.h"
+
+#include "MUQ/SamplingAlgorithms/MHKernel.h"
+#include "MUQ/SamplingAlgorithms/MHProposal.h"
+#include "MUQ/SamplingAlgorithms/CrankNicolsonProposal.h"
+#include "MUQ/SamplingAlgorithms/SamplingProblem.h"
+#include "MUQ/SamplingAlgorithms/SubsamplingMIProposal.h"
+
+#include "MUQ/SamplingAlgorithms/MIComponentFactory.h"
+
+#include <boost/property_tree/ptree.hpp>
+
+namespace pt = boost::property_tree;
+using namespace muq::Modeling;
+using namespace muq::SamplingAlgorithms;
+using namespace muq::Utilities;
+
+#include "MUQ/SamplingAlgorithms/ParallelMIMCMCWorker.h"
+#include "MUQ/SamplingAlgorithms/ParallelFixedSamplesMIMCMC.h"
+
+#include "Problem.h"
+
+
+int main(int argc, char **argv){
+
+  spdlog::set_level(spdlog::level::debug);
+
+  MPI_Init(&argc, &argv);
+
+  pt::ptree pt;
+  pt.put("MCMC.NumSamples", 1e3); // number of samples for single level
+  pt.put("MCMC.burnin", 1e1); // number of samples for single level
+  pt.put("MLMCMC.Subsampling", 5);
+
+  auto comm = std::make_shared<parcer::Communicator>();
+
+
+  auto componentFactory = std::make_shared<MyMIComponentFactory>(pt);
+  StaticLoadBalancingMIMCMC parallelMIMCMC (pt, componentFactory);
+
+  if (comm->GetRank() == 0) {
+    spdlog::info("Starting parallel run");
+    parallelMIMCMC.Run();
+    spdlog::info("Parallel run finished");
+    Eigen::VectorXd meanQOI = parallelMIMCMC.MeanQOI();
+    std::cout << "mean QOI: " << meanQOI.transpose() << std::endl;
+  }
+  parallelMIMCMC.Finalize();
+
+  MPI_Finalize();
+}
