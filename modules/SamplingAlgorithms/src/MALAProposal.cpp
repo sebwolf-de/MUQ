52efecea0d30e2fec07572e0a265790cd4d0ba13
Matthew Parno <Matthew.D.Parno@erdc.dren.mil>
2020-06-04 11:12:56 -0400

    Added infMala proposal.

--- modules/SamplingAlgorithms/src/MALAProposal.cpp
+++ modules/SamplingAlgorithms/src/MALAProposal.cpp
@@ -48,19 +48,21 @@ std::shared_ptr<SamplingState> MALAProposal::Sample(std::shared_ptr<SamplingStat
   Eigen::VectorXd const& xc = currentState->state.at(blockInd);
 
   // Get the gradient from the previous step
-  Eigen::VectorXd grad; // vector holding the gradient of the log density
   Eigen::VectorXd sigmaGrad; // vector holding the covariance of the proposal times the gradient
 
   std::stringstream blockId;
   blockId << "_" << blockInd;
 
-  if(!currentState->HasMeta("GradLogDensity" + blockId.str()))
-    currentState->meta["GradLogDensity" + blockId.str()] = prob->GradLogDensity(currentState, blockInd);
+  if(!currentState->HasMeta("MALA_SigmaGrad" + blockId.str())){
+    Eigen::VectorXd grad; // vector holding the gradient of the log density
 
-  grad = AnyCast( currentState->meta["GradLogDensity" + blockId.str()] );
+    if(!currentState->HasMeta("GradLogDensity" + blockId.str()))
+      currentState->meta["GradLogDensity" + blockId.str()] = prob->GradLogDensity(currentState, blockInd);
 
-  if(!currentState->HasMeta("MALA_SigmaGrad" + blockId.str()))
+    grad = AnyCast( currentState->meta["GradLogDensity" + blockId.str()] );
+
     currentState->meta["MALA_SigmaGrad" + blockId.str()] = zDist->ApplyCovariance(grad).col(0).eval();
+  }
 
   sigmaGrad = AnyCast(currentState->meta["MALA_SigmaGrad" + blockId.str()]);
 
