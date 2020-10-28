7efdbdab0acb03aea938d50bcfb5b4358bddf40d
Matthew Parno <Matthew.D.Parno@erdc.dren.mil>
2020-10-28 10:35:07 -0400

    Fixed another ODE name in python wrapper.

--- modules/Modeling/python/ODEWrapper.cpp
+++ modules/Modeling/python/ODEWrapper.cpp
@@ -2,14 +2,14 @@
 
 #include "MUQ/Utilities/PyDictConversion.h"
 
-#include "MUQ/Modeling/ODEPiece.h"
+#include "MUQ/Modeling/ODE.h"
 
 using namespace muq::Modeling;
 using namespace muq::Utilities;
 namespace py = pybind11;
 
 void PythonBindings::ODEWrapper(py::module &m) {
-  py::class_<ODEPiece, ModPiece, std::shared_ptr<ODEPiece> > ode(m, "ODEPiece");
-  ode.def(py::init( [] (std::shared_ptr<ModPiece> const& rhs, Eigen::VectorXd const& times, py::dict const& d) { return new ODEPiece(rhs, times, ConvertDictToPtree(d)); }), py::keep_alive<1, 2>());
+  py::class_<ODE, ModPiece, std::shared_ptr<ODE> > ode(m, "ODE");
+  ode.def(py::init( [] (std::shared_ptr<ModPiece> const& rhs, Eigen::VectorXd const& times, py::dict const& d) { return new ODE(rhs, times, ConvertDictToPtree(d)); }), py::keep_alive<1, 2>());
 
 }
