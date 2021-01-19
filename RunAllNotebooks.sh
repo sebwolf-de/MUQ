4397d8169ef4f6f141b97316aed991e74defa8a6
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2021-01-19 10:35:23 +0100

    Improved notebook runner script

--- RunAllNotebooks.sh
+++ RunAllNotebooks.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
+exit_code=0
+
 # Make sure globstar is enabled
 shopt -s globstar
-for i in **/*.ipynb; do # Whitespace-safe and recursive
+for notebook in **/*.ipynb; do # Whitespace-safe and recursive
     echo "======================"
-    echo "Running notebook $i..."
+    echo "Running $notebook:"
+    echo ""
 
-    papermill --cwd "$(dirname "$i")" "$i" -
+    papermill --cwd "$(dirname "$notebook")" "$notebook" -
 
-    echo "======================"
+    papermill_exit=$?
+
+    if [ $papermill_exit -eq 0 ]
+    then
+        echo "Notebook $notebook successful."
+    else
+        echo "Notebook $notebook FAILED!"
+    fi
+
+    exit_code=$(($exit_code + $papermill_exit)) # Simply add exit codes; will be zero if all tests successful
 done
 
-
+exit $exit_code
